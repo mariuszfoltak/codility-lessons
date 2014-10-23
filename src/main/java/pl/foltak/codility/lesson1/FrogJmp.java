881167f29fe4cbf2f0cb1bfaa9a4b90425f3536c
Mariusz Foltak <mariusz.foltak@coi.gov.pl>
2014-10-23 15:00:04 +0200

    Refactor

--- src/main/java/pl/foltak/codility/lesson1/FrogJmp.java
+++ src/main/java/pl/foltak/codility/lesson1/FrogJmp.java
@@ -1,9 +1,5 @@
 package pl.foltak.codility.lesson1;
 
-/**
- *
- * @author Mariusz Foltak <mariusz.foltak@coi.gov.pl>
- */
 public class FrogJmp {
 
     public int solution(int X, int Y, int D) {
--- src/test/java/pl/foltak/codility/lesson1/FrogJmpTest.java
+++ src/test/java/pl/foltak/codility/lesson1/FrogJmpTest.java
@@ -1,18 +1,13 @@
-
 package pl.foltak.codility.lesson1;
 
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author Mariusz Foltak <mariusz.foltak@coi.gov.pl>
- */
 public class FrogJmpTest {
-    
+
     private FrogJmp instance;
-    
+
     @Before
     public void setUp() {
         instance = new FrogJmp();
@@ -25,12 +20,12 @@ public class FrogJmpTest {
 
     @Test
     public void testFrogPositonEqual() {
-        assertEquals(2, instance.solution( 0, 60, 30));
+        assertEquals(2, instance.solution(0, 60, 30));
     }
 
     @Test
     public void testFrogAlreadyOnTargetPositoin() {
         assertEquals(0, instance.solution(10, 10, 5));
     }
-    
+
 }
