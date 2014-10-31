7f8bb409d30a891d283eea18a784711e2ff112a0
Mariusz Foltak <mariusz.foltak@coi.gov.pl>
2014-10-31 13:05:05 +0100

    Added solution for L2 - MissingInteger

---
+++ src/main/java/pl/foltak/codility/lesson2/MissingInteger.java
@@ -0,0 +1,27 @@
+package pl.foltak.codility.lesson2;
+
+/**
+ *
+ * @author Mariusz Foltak <mariusz.foltak@coi.gov.pl>
+ */
+public class MissingInteger {
+
+    public int solution(int[] A) {
+        int max = A.length + 1;
+        boolean[] integers = new boolean[max];
+
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] > 0 && A[i] < max) {
+                integers[A[i] - 1] = true;
+            }
+        }
+
+        for (int i = 0; i < integers.length; i++) {
+            if (!integers[i]) {
+                return i + 1;
+            }
+        }
+        return -1;
+    }
+
+}
---
+++ src/test/java/pl/foltak/codility/lesson2/MissingIntegerTest.java
@@ -0,0 +1,79 @@
+package pl.foltak.codility.lesson2;
+
+import java.util.Random;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Mariusz Foltak <mariusz.foltak@coi.gov.pl>
+ */
+public class MissingIntegerTest {
+
+    private MissingInteger instance;
+
+    @Before
+    public void setUp() {
+        instance = new MissingInteger();
+    }
+
+    @Test
+    public void testAscendingSortedNumbers() {
+        int[] array = {-5, -4, -3, -1, 0, 1, 2, 3, 4, 5, 6};
+        assertEquals("It isn't minimal positive integer", 7, instance.solution(array));
+    }
+
+    @Test
+    public void testDescendingSortedNumbers() {
+        int[] array = {5, 4, 3, 1, 0, -1, -2, -3, -6};
+        assertEquals("It isn't minimal positive integer", 2, instance.solution(array));
+    }
+
+    @Test
+    public void testOnlyNonNegativeNumbers() {
+        int[] array = {4, 20, 2, 5, 1, 3, 5, 0};
+        assertEquals("It isn't minimal positive integer", 6, instance.solution(array));
+    }
+
+    @Test
+    public void testOnlyNonPositiveNumbers() {
+        int[] array = {-10, -2, -7, -3, -4, -7, -1};
+        assertEquals("It isn't minimal positive integer", 1, instance.solution(array));
+    }
+
+    @Test
+    public void testBigNumbersFromOne() {
+        int[] array = new int[100000];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+        }
+        assertEquals("It isn't minimal positive integer", 100001, instance.solution(array));
+    }
+
+    @Test
+    public void testBigNumbersWithoutOne() {
+        int[] array = new int[1000000];
+        for (int i = 900000; i < array.length; i++) {
+            array[i] = i + 1;
+        }
+        assertEquals("It isn't minimal positive integer", 1, instance.solution(array));
+    }
+
+    @Test
+    public void testRandomBigNumbers() {
+        Random r = new Random();
+        int[] array = new int[1000000];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = r.nextInt() * (r.nextInt(1) - 1);
+            if (array[i] == 4) {
+                array[i] = 5;
+            }
+        }
+        array[43456] = 1;
+        array[11723] = 2;
+        array[22485] = 3;
+        assertEquals("It isn't minimal positive integer", 4, instance.solution(array));
+    }
+
+}
